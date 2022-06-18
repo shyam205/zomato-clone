77af62c0973891cd7efcf34920140407fd0f6d2c
shyam205 <mastershyam2013@gmail.com>
2022-06-18 21:55:50 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,18 @@
+export const zomatofeatures = [
+    {
+
+       image:"https://b.zmtcdn.com/webFrontend/e5b8785c257af2a7f354f1addaf37e4e1647364814.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*",
+       title:"Order Online",
+       description:"Stay home and order to your doorstep"
+    },
+    {
+       image:"https://b.zmtcdn.com/webFrontend/d026b357feb0d63c997549f6398da8cc1647364915.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*",
+       title:"Dining Out",
+       description:"View the city's favourite dining venues"
+    },
+    {
+        image:"https://b.zmtcdn.com/webFrontend/d9d80ef91cb552e3fdfadb3d4f4379761647365057.jpeg?output-format=webp&fit=around|402:360&crop=402:360;*,*",
+        title:"Nightlife and Clubs",
+        description:"Explore the city’s top nightlife outlets"
+    }
+]
