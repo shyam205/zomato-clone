9a4ab8c927e1e7471730f93c09c9d366069a5bd8
shyam205 <mastershyam2013@gmail.com>
2022-07-03 23:39:02 +0530

    added mobile page

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function Home() {
              <div className={styles.header}>
                  <div className='header-left-section'>
                  <svg xmlns="http://www.w3.org/2000/svg" fill="#ffffff" style={{"height":"15px"}} viewBox="0 0 384 512"><path d="M304 0h-224c-35.35 0-64 28.65-64 64v384c0 35.35 28.65 64 64 64h224c35.35 0 64-28.65 64-64V64C368 28.65 339.3 0 304 0zM192 480c-17.75 0-32-14.25-32-32s14.25-32 32-32s32 14.25 32 32S209.8 480 192 480zM304 64v320h-224V64H304z"/></svg>
-                 <span className={styles.apptext}>Get The App</span>
+                 <Link href="/mobile"><span className={styles.apptext}>Get The App</span></Link>
                  </div>
                  <div className='header-right-section'>
                    <nav>
